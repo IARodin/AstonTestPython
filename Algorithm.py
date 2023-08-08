cabae8a0ff9ff1626024d993cd9dfa0b9547e4ce
IARodin <71449805+IARodin@users.noreply.github.com>
2023-08-08 20:47:30 +0700

    Initial commit

---
+++ Algorithm.py
@@ -0,0 +1,6 @@
+number = int(input("Введите число: "))
+
+if number > 7:
+    print("Привет")
+else:
+    print("Число меньше или равно 7")
---
+++ Algorithm3.py
@@ -0,0 +1,5 @@
+numbers = {5, 9, 12, 15, 18, 21, 25, 30}
+
+for numb in numbers:
+    if numb % 3 == 0:
+        print(f"Число - {numb} кратно 3.")
